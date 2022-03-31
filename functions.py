cee489cf8e5a25b7967ad5fb5152d4fb9eab754c
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-03-30 21:12:28 -0600

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,2 +1,20 @@
 def multiply(a, b):
   return a*b
+
+def divide(a, b):
+  return a/b
+
+def integer_divide(a, b):
+  return a//b
+
+def add(a, b):
+  return a+b
+
+def subtract(a, b):
+  return a-b
+
+print(multiply(2,5))
+print(divide(10,3))
+print(integer_divide(10,3))
+print(add(2,40))
+print(subtract(5,2))
