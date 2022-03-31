edb66b57ebddf66d15388c7459b758b5e8797a89
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-03-30 21:31:04 -0600

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,18 @@
+def for_loop(n):
+  test_str = "Hello World!"
+  for char in test_str:
+    print(char)
+
+def while_loop(n):
+  index = 0
+  while index < len(test_str):
+    print(test_str[index])
+    index += 1
+    
+def range_for_loop(n):
+  for num in range(1,n+1,2)
+    print(num)
+    
+for_loop(10)
+while_loop(10)
+range_for_loop(20)
