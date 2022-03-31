4a59ea7af67076aa9f3b09e584b86617e84d21df
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-03-30 21:39:12 -0600

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,12 @@
+test_string = "Hello World!
+
+def recurse_this(n):
+  if n == -1:
+    print(test_str[10 - n])
+    print("This is the base case!")
+  else:
+    print(test_str[10 - n])
+    #print("iteration: " + str(n))
+    recurse_this(n-1)
+
+recurse_this(len(test_str))
