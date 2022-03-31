4146f2cc6f933c0b311dbb16af05d69c400cb943
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-03-30 20:56:44 -0600

    Update pv.py

--- pv.py
+++ pv.py
@@ -4,7 +4,7 @@ example_int = 5
 example_float = 10.0
 example_boolean = True
 
-print(type(example_string)
+#print(type(example_string)
 print(type(example_char)
 print(type(example_int)
 print(type(example_float)
