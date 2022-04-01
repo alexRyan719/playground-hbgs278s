dec4eefd601559c8cbc2afc87213d3f4822a4377
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-03-31 20:58:01 -0600

    Update control_flow_2.py

--- control_flow_2.py
+++ control_flow_2.py
@@ -1,5 +1,6 @@
 
 the_meaning_of_life = 42
+valid = False
 
 if the_meaning_of_life == 42:
   print("Ran")
@@ -7,3 +8,7 @@ if the_meaning_of_life == 42:
 if 1:
   print("1 worked as well!")
  
+if valid:
+  print("It's valid")
+else:
+  print("It's not valid")
