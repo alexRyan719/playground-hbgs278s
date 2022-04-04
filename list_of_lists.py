798548f4212359e092f736af7d8f18a01faba107
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-04-03 20:40:46 -0600

    Update list_of_lists.py

--- list_of_lists.py
+++ list_of_lists.py
@@ -1,13 +1,27 @@
 my_list = []
 my_list_of_lists = []
 
-width = 10
-height = 10
+width = 7
+height = 5
 
-for num in range(width):
-  for n in range(height):
+for num in range(height):
+  for n in range(width):
     my_list.append(n)
   my_list_of_lists.append(my_list)
   my_list = []
-  
-print(my_list_of_lists)
+
+for item in my_list_of_lists:  
+    print(item)
+    print()
+
+x = 0
+y = 0
+
+# Try adjusting the coordinates by adjusting x and y. 
+print(my_list_of_lists[y][x+n])
+
+n = 3
+n_2 = 2
+
+# Or, you can change n or n_2 to change coordinates.
+print(my_list_of_lists[y+n][x+n_2])
