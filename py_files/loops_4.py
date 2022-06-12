cbbf0c1a42d52c8b95beff343a45d25edcf002c9
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-06-12 15:17:47 -0600

    Update loops_4.py

--- py_files/loops_4.py
+++ py_files/loops_4.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+# Multiplication
+to_mult = 5
+mult_times = 10
+mult_ans = to_mult
+
+for num_m in range(1,mult_times):
+    mult_ans += to_mult
+
+print(mult_ans)
+
+# Division
+to_div = 5
+div_times = 10
+div_ans = to_mult
+
+for num_d in range(1,div_times):
+    div_ans += to_div
+
+print(div_ans)
