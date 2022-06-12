82e989c790442d68be13386f863bd4833768fc4a
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-06-12 15:18:37 -0600

    Update loops_4.py

--- py_files/loops_4.py
+++ py_files/loops_4.py
@@ -9,11 +9,11 @@ for num_m in range(1,mult_times):
 print(mult_ans)
 
 # Division
-to_div = 5
-div_times = 10
+to_div = 50
+div_times = 5
 div_ans = to_mult
 
 for num_d in range(1,div_times):
-    div_ans += to_div
+    div_ans -= to_div
 
 print(div_ans)
