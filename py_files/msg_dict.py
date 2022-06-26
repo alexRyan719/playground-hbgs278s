2d91e7c57e1642ce67060979cc4c90d9158f3ac2
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-06-26 09:39:24 -0600

    Create msg_dict.py

---
+++ py_files/msg_dict.py
@@ -0,0 +1,9 @@
+msg_dict = {
+  1:"Nice weather we're having!", 2:"Careful warrior...", 3:"Welcome to my blacksmith shop!", -1:"Watch it there..."  
+}
+
+def show_all_msg():
+  for num in msg_dict:
+    print(msg_dict(num)
+          
+show_all_msg()
