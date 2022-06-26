8726c4cf349b98db1fc5b332f1225a35fd8e6dd5
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2022-06-26 11:57:59 -0600

    Update msg_dict.py

--- py_files/msg_dict.py
+++ py_files/msg_dict.py
@@ -1,6 +1,10 @@
 msg_dict = {
-  1:"Nice weather we're having!", 2:"Careful warrior...", 3:"Welcome to my blacksmith shop!", -1:"Watch it there...", "h_1":"Very spoooooky weather we're having!",
-  "c_1":"Very frostie weather we're having!", "v_1":"Very bubbly weather we're having!"  
+  1:"Nice weather we're having!", 2:"Careful warrior...", 3:"Welcome to my blacksmith shop!", -1:"Watch it there...", 
+  "h_1":"Very spoooooky weather we're having!",
+  "c_1":"Very frostie weather we're having!", 
+  "v_1":"Very bubbly weather we're having!",
+  "eZ":"Hasta la vista, baby"
+  "aB":"Groovy!"
 }
 
 def show_all_msg():
