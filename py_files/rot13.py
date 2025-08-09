ca1a9002b9c187fd6949ac66b90368f44cef78f2
Alex Ryan <87581318+alexRyan719@users.noreply.github.com>
2025-08-09 17:00:01 -0600

    Update rot13.py
    More debugging

--- py_files/rot13.py
+++ py_files/rot13.py
@@ -1,9 +1,11 @@
 def rot13(plain_text):
+    
     alphabet_dict  = {
         'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10
         ,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18
         ,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26
     } 
+    
     number_dict  = {
         1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'j'
         ,11:'k',12:'l',13:'m',14:'n',15:'o',16:'p',17:'q',18:'r'
@@ -47,7 +49,8 @@ def rot13(plain_text):
         # If the new index is larger than 26, wrap around from the beginning
         if new_index > 26:
             new_index %= 26
-          
+
+        # Build the return string based on if capitalized and found
         if capitalized and found:
             cipher_text += number_dict[new_index].upper()
         elif found:
@@ -70,7 +73,7 @@ def rot13(plain_text):
         
         capitalized = False
         found = False
-    print("DEBUG")
+    #print("DEBUG")
     
     return cipher_text
 
